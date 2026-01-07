98dcbe2cb37d4f4d8da3f73be49d13043f82b3dd
Lingeshwarma.M.K <lingeshwarma108@gmail.com>
2026-01-07 17:18:23 +0530

    Fix ESLint indentation in ProjectGrid and Work section

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -24,7 +24,7 @@ const Contact = () => {
                     <AnimatedWords
                         title={"contact"}
                         style={
-                            "flex max-w-[250px] flex-col items-start text-left text-[100px] font-extrabold uppercase leading-[0.9em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center sm:text-[150px] md:text-[150px] lg:text-center lg:text-[120px] xl:text-[250px]"
+                            "flex max-w-[250px] flex-col items-start text-left text-[70px] font-extrabold uppercase leading-[1.3em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center sm:text-[250px] md:text-[250px] lg:text-center lg:text-[250px] xl:text-[250px]"
                         }
                     />
                 </div>
